52df198631cb93755711289f515867ce5568a008
Abhkk9 <Abhshk.k98@gmail.com>
2025-01-15 15:51:56 +0530

    app without components

--- MegaBlog/MegaBlogAppWrite/src/App.jsx
+++ MegaBlog/MegaBlogAppWrite/src/App.jsx
@@ -4,27 +4,37 @@ import authService from "./appwrite/auth";
 import {login,  logOut} from "./store/authSlice";
 import { useState,useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+// import { Footer,Header } from './components';
 
 function App() {
   const [loading, setLoading]= useState(true);
-  // const dispatch =useDispatch();
+  const dispatch =useDispatch();
 
-  // useEffect(()=>{
-  //   authService.getCurrentUser()
-  //   .then((userData)=>{
-  //     if(userData){
-  //       dispatch(login({userData}) )
-  //     }
-  //     else{
-  //       dispatch(logOut())
-  //     }
-  //   })
-  //   .finally( () =>setLoading(false))
-  // },[])
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}) )
+      }
+      else{
+        dispatch(logOut())
+      }
+    })
+    .finally( () =>setLoading(false))
+  },[])
 
   return !loading?(
-    <div className='min-h-sc'>
+    <div className='min-h-screen flex flex-wrap '>
       <h1> A REACT APP WITH APPWRITE</h1>
+      <div className='w-full block'>
+      {/* <Header/> */}
+      <main>
+        {/* <Outlet /> */}
+        <h1>main</h1>
+      </main>
+      {/* <Footer /> */}
+      </div>
     </div>
   ):(<div>default</div>)  
 }
